adddfe943652af26b2e30f98c7c7d0b734fa88db
Gilad Shaham <gilads@iguazio.com>
2020-04-03 18:00:19 +0300

    - Updated additional demos from mlrun/demos - removed reference to the location-based recommendation demo

---
+++ get-additional-demos.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+USER=${V3IO_USERNAME}
+DEST_DIR="/v3io/users/${USER}/demos"
+TEMP_DIR="${DEST_DIR}/temp-demos"
+GIT_REPO="https://github.com/mlrun/demos"
+
+git clone ${GIT_REPO} --branch master --single-branch --depth 1 ${TEMP_DIR}
+
+echo "Copying files to ${DEST_DIR}..."
+mv ${TEMP_DIR}/README.md ${TEMP_DIR}/README-MLRUN.md
+cp -r ${TEMP_DIR}/* ${DEST_DIR}
+
+echo "Deleting temporary ${TEMP_DIR} directory ..."
+rm -rf ${TEMP_DIR}
+
+echo "DONE"
