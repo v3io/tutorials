71c2063df20292c68b6e5206f8ad55326917c6c6
Gilad Shaham <gilads@iguazio.com>
2020-08-05 01:31:26 +0300

    include notebook fixes

--- demos/rapid-streaming-ml/config.py
+++ demos/rapid-streaming-ml/config.py
@@ -1,6 +1,5 @@
 # %load config.py
 from os import getenv, path
-import json
 import os
 
 
@@ -10,9 +9,7 @@ CONTAINER = 'users'
 BATCH_SIZE = 1024
 WEB_API = "http://v3io-webapi:8081"
 WEB_API_USERS = "{0}/{1}/".format(WEB_API, CONTAINER)
-PROJECT_NAME = "arthur-prototype"
-PATH = "/examples/{0}/events-pq".format(PROJECT_NAME)
-EVENTS_TARGET_PATH = "/User{0}".format(PATH)
+PROJECT_NAME = "model-deployment-with-streaming"
 STREAM_CONFIGS = {'generated-stream': {
                         'path': path.join(V3IO_USERNAME, 'examples/{0}/generated-stream'.format(PROJECT_NAME)),
                         'shard_count': 8},
@@ -34,6 +31,6 @@ STREAM_CONFIGS = {'generated-stream': {
                   }
                  }
 PARTITION_ATTR = "user_id"
-ENRICHMENT_TABLE_PATH = '/examples/{0}/enrichment-table'.format(PROJECT_NAME)
-FEATURE_TABLE_PATH = path.join(V3IO_USERNAME, '/examples/{0}/feature-table'.format(PROJECT_NAME))
-MDWS_S2P_TARGET_PATH = EVENTS_TARGET_PATH
+ENRICHMENT_TABLE_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'enrichment-table')
+FEATURE_TABLE_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'feature-table')
+MDWS_S2P_TARGET_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'events-pq')
