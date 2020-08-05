152e945feb55bd8b68e63515fe3f3d9d05822f59
Gilad Shaham <gilads@iguazio.com>
2020-08-05 10:12:20 +0300

    fix additional paths

--- demos/rapid-streaming-ml/config.py
+++ demos/rapid-streaming-ml/config.py
@@ -10,27 +10,28 @@ BATCH_SIZE = 1024
 WEB_API = "http://v3io-webapi:8081"
 WEB_API_USERS = "{0}/{1}/".format(WEB_API, CONTAINER)
 PROJECT_NAME = "model-deployment-with-streaming"
+DATA_PATH = path.join(V3IO_USERNAME, 'examples',PROJECT_NAME, 'data')
 STREAM_CONFIGS = {'generated-stream': {
-                        'path': path.join(V3IO_USERNAME, 'examples/{0}/generated-stream'.format(PROJECT_NAME)),
+                        'path': path.join(DATA_PATH, 'generated-stream'),
                         'shard_count': 8},
                   'incoming-events-stream': {
-                        'path': path.join(V3IO_USERNAME, 'examples/{0}/incoming-events-stream'.format(PROJECT_NAME)),
+                        'path': path.join(DATA_PATH, 'incoming-events-stream'),
                         'shard_count': 8
                   },
                   'enriched-events-stream': {
-                        'path': path.join(V3IO_USERNAME, 'examples/{0}/enriched-events-stream'.format(PROJECT_NAME)),
+                        'path': path.join(DATA_PATH, 'enriched-events-stream'),
                         'shard_count': 8
                   },
                   'serving-stream': {
-                        'path': path.join(V3IO_USERNAME, 'examples/{0}/serving-stream'.format(PROJECT_NAME)),
+                        'path': path.join(DATA_PATH, 'serving-stream'),
                         'shard_count': 8
                   },
                   'inference-stream': {
-                        'path': path.join(V3IO_USERNAME, 'examples/{0}/inference-stream'.format(PROJECT_NAME)),
+                        'path': path.join(DATA_PATH, 'inference-stream'),
                         'shard_count': 8
                   }
                  }
 PARTITION_ATTR = "user_id"
-ENRICHMENT_TABLE_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'enrichment-table')
-FEATURE_TABLE_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'feature-table')
-MDWS_S2P_TARGET_PATH = path.join(os.sep, V3IO_USERNAME, 'examples', PROJECT_NAME, 'events-pq')
+ENRICHMENT_TABLE_PATH = path.join(DATA_PATH, 'enrichment-table')
+FEATURE_TABLE_PATH = path.join(DATA_PATH, 'feature-table')
+PARQUET_TARGET_PATH = path.join(DATA_PATH, 'events-pq')
