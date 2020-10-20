90c3653bb0527744e663e710807a3f9905e24e0a
Gilad Shaham <gilads@iguazio.com>
2020-10-20 06:45:27 +0300

    get-additional-demos.sh renames existing non-git demos folder to demos.old

--- get-additional-demos.sh
+++ get-additional-demos.sh
@@ -8,23 +8,25 @@ git_clone_or_pull() {
     REPOSRC="${1}"
     LOCALREPO="${2}"
 
-    LOCALREPO_GIT_DIR="$LOCALREPO/.git"
+    LOCALREPO_GIT_DIR="${LOCALREPO}/.git"
 
-    echo "Reading $REPOSRC to $LOCALREPO" 
+    echo "Reading ${REPOSRC} to '${LOCALREPO}' folder..." 
 
-    if [ -d "$LOCALREPO_GIT_DIR" ]; then
-        git -C "$LOCALREPO" pull origin
-    elif [ -d "$LOCALREPO" ]; then
+    if [ -d "${LOCALREPO_GIT_DIR}" ]; then
+        git -C "${LOCALREPO}" pull origin
+    elif [ -d "${LOCALREPO}" ]; then
         if [ -z ${3} ]; then
             echo "'${LOCALREPO}' folder exists, but is not a git repository. Aborting." 1>&2
             exit 1
         else
-            echo "'${LOCALREPO}' folder exists, but is not a git repository, switching to '${3}' instead."
-            git_clone_or_pull "$REPOSRC" "${3}"
+            echo "'${LOCALREPO}' folder exists, but is not a git repository, moving '${LOCALREPO}' to '${3}'."
+            cp -r "${LOCALREPO}" "${3}"
+            rm -r "${LOCALREPO}"
+            git clone "${REPOSRC}" "${LOCALREPO}"
         fi
     else
-        git clone "$REPOSRC" "$LOCALREPO"
+        git clone "${REPOSRC}" "${LOCALREPO}"
     fi
 }
 
-git_clone_or_pull https://github.com/mlrun/demos.git demos mlrun-demos
+git_clone_or_pull https://github.com/mlrun/demos.git demos demos.old
